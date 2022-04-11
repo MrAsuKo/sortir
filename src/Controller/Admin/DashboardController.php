5baf100edbb2e87645f03789e40cda6248149ed9
aveslin2021 <veslinalexis44@gmail.com>
2022-04-11 14:14:09 +0200

    Connection correction

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Participant;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -13,51 +14,19 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
-
         return $this->render('admin/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Sortir')
-
-
-            // you can include HTML contents too (e.g. to link to an image)
-            ->setTitle('<img src="..."> ACME <span class="text-small">Corp.</span>')
-
-            // the path defined in this method is passed to the Twig asset() function
-            ->setFaviconPath('favicon.svg')
-
-            // the domain used by default is 'messages'
-            ->setTranslationDomain('my-custom-domain')
-
-            // there's no need to define the "text direction" explicitly because
-            // its default value is inferred dynamically from the user locale
-            ->setTextDirection('ltr')
-
-            // set this option if you prefer the page content to span the entire
-            // browser width, instead of the default design which sets a max width
-            ->renderContentMaximized()
-
-            // set this option if you prefer the sidebar (which contains the main menu)
-            // to be displayed as a narrow column instead of the default expanded design
-            ->renderSidebarMinimized()
-
-            // by default, users can select between a "light" and "dark" mode for the
-            // backend interface. Call this method if you prefer to disable the "dark"
-            // mode for any reason (e.g. if your interface customizations are not ready for it)
-            ->disableDarkMode()
-
-            // by default, all backend URLs are generated as absolute URLs. If you
-            // need to generate relative URLs instead, call this method
-            ->generateRelativeUrls();
+            ->setTitle('Sortir');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Participant', "",Participant::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
