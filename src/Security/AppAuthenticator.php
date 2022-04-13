6989a7dc9c3af6f4cd4e67b86b0606a7daeeb5a1
ljousselin2021 <lenaic.jousselin2021@campus-eni.fr>
2022-04-13 15:02:38 +0200

    Redirection si pseudo vide

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -49,7 +49,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('app_accueil'));
+        return new RedirectResponse($this->urlGenerator->generate('loginDirection'));
 
     }
 
