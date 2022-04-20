889da6d5ccd3edbf63b5c86e9b2266bfc2e1891f
ljousselin2021 <lenaic.jousselin2021@campus-eni.fr>
2022-04-20 08:57:40 +0200

    2eme theme

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -4,28 +4,26 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Cookie;
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ThemeController extends AbstractController
 {
+
     #[Route('/theme', name: 'theme_choix')]
     public function choix(
         Request $request
     ): Response
     {
 
-        $theme = $request->query->get('theme-select');
-        dd($theme);
-        $cookie = new Cookie('theme', //Nom cookie
-            '$theme', //Valeur
-            strtotime('tomorrow'), //expire le
-            '/', //Chemin de serveur
-            'stacktraceback.com', //Nom domaine
-            true, //Https seulement
-            true);
+        $theme = $request->query->get('themes');
+        dump($theme);
+        $response = New Response();
+            $cookie = new Cookie('theme', $theme , strtotime('tomorrow'));
+        $response->headers->setCookie($cookie);
+        $response->send();
+
 
         return $this->redirectToRoute('app_accueil');
     }
---
+++ src/Form/ThemeType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ThemeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Theme',EntityType::class,
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
